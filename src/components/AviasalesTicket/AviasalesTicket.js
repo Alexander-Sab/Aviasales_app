877ffae3433323c4c7b64add2636a0f8192ca536
Alexander-Sab <113023875+Alexander-Sab@users.noreply.github.com>
2023-11-05 16:38:31 +0300

    Ваше сообщение коммита

--- src/components/AviasalesTicket/AviasalesTicket.js
+++ src/components/AviasalesTicket/AviasalesTicket.js
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
-import React, { useEffect } from 'react'
+import { format, add } from 'date-fns'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { getTickets } from '../../store/actions'
@@ -7,75 +8,210 @@ import { getTickets } from '../../store/actions'
 import classes from './AviasalesTicket.module.scss'
 
 export const AviasalesTicket = () => {
-  const tickets = useSelector((state) => state.tickets)
+  const { tickets } = useSelector((state) => state.tickets)
+  const [isTicketsLoading, setIsTicketsLoading] = useState(true)
   console.log(tickets)
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(getTickets())
-  }, [dispatch])
-  return (
-    <section className={clsx(classes.aviasales__ticket, classes.ticket)}>
-      <div className={clsx(classes.ticket__header)}>
-        <div className={clsx(classes.ticket__price)}>ticket.price</div>
-        <img
-          className={clsx(classes.ticket__img)}
-          src="https://pics.avs.io/99/36/UT.png"
-          alt="Airline logo"
-        />
-      </div>
-      <div className={clsx(classes.ticket__section)}>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            MOW – HKT
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            10:45 – 08:00
-          </div>
-        </div>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            В пути
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            21ч 15м
-          </div>
-        </div>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            2 пересадки
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            HKG, JNB
-          </div>
-        </div>
-      </div>
+    const fetchTickets = async () => {
+      try {
+        dispatch(getTickets())
+        setIsTicketsLoading(false)
+      } catch (error) {
+        console.error(error)
+        setIsTicketsLoading(false)
+      }
+    }
 
-      <div className={clsx(classes.ticket__section)}>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            MOW – HKT
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            11:20 – 00:500
-          </div>
+    fetchTickets()
+  }, [dispatch])
+
+  const firstFiveTickets = tickets.slice(0, 5) // Ограничение до первых пяти билетов
+  // фильтр
+  const [selectedFilters, setSelectedFilters] = useState({
+    all: true,
+    withoutTransfers: true,
+    oneTransfer: true,
+    twoTransfers: true,
+    threeTransfers: true,
+  })
+
+  const [sortType, setSortType] = useState('default')
+  // Фильтрация билетов
+  const filteredTickets = tickets.filter((ticket) => {
+    // Применение фильтров
+    if (selectedFilters.all) {
+      return true
+    }
+    return selectedFilters[ticket.transfers.length]
+  })
+
+  // Сортировка билетов
+  const sortedTickets = filteredTickets.sort((a, b) => {
+    if (sortType === 'price') {
+      return a.price - b.price
+    } else if (sortType === 'duration') {
+      return (
+        a.segments.reduce(
+          (totalDuration, segment) => totalDuration + segment.duration,
+          0,
+        ) -
+        b.segments.reduce(
+          (totalDuration, segment) => totalDuration + segment.duration,
+          0,
+        )
+      )
+    }
+    return 0
+  })
+
+  // Отображение билетов
+
+  return (
+    <>
+      {firstFiveTickets.length === 0 ? (
+        <div className={clsx(classes['aviasales__ticket-error'])}>
+          <div className={clsx(classes['viasales__ticket-error__icons'])}></div>
+          <p className={clsx(classes['aviasales__ticket-error__text'])}>
+            К сожалению, билетов по вашему запросу не найдено. <br /> Попробуйте
+            изменить параметры поиска или повторить позже.
+          </p>
         </div>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            В пути
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            13ч 30м
-          </div>
-        </div>
-        <div className={clsx(classes['ticket__section-info'])}>
-          <div className={clsx(classes['ticket__section-info--name'])}>
-            1 пересадки
-          </div>
-          <div className={clsx(classes['ticket__section-info--value'])}>
-            HKG
-          </div>
-        </div>
-      </div>
-    </section>
+      ) : (
+        <>
+          {firstFiveTickets.map((ticket) => {
+            // Время 1
+            const departureTime1 = format(
+              new Date(ticket.segments[0].date),
+              'HH:mm',
+            )
+            const arrivalTime1 = format(
+              add(new Date(ticket.segments[1].date), {
+                minutes: ticket.segments[1].duration,
+              }),
+              'HH:mm',
+            )
+            const formattedTimeRange1 = `${departureTime1} – ${arrivalTime1}`
+            const departureTime2 = format(
+              new Date(ticket.segments[1].date),
+              'HH:mm',
+            )
+            const arrivalTime2 = format(
+              add(new Date(ticket.segments[1].date), {
+                minutes: ticket.segments[1].duration,
+              }),
+              'HH:mm',
+            )
+            const formattedTimeRange2 = `${departureTime2} – ${arrivalTime2}`
+            // Время 2
+            const duration1 = ticket.segments[0].duration
+            const hours1 = Math.floor(duration1 / 60)
+            const minutes1 = duration1 % 60
+            const formattedDuration1 = `${hours1}ч ${minutes1}м`
+            const duration2 = ticket.segments[0].duration
+            const hours2 = Math.floor(duration2 / 60)
+            const minutes2 = duration2 % 60
+            const formattedDuration2 = `${hours2}ч ${minutes2}м`
+
+            return (
+              <section
+                key={`${ticket.price}`}
+                className={clsx(classes.aviasales__ticket, classes.ticket)}
+              >
+                <div className={clsx(classes.ticket__header)}>
+                  <div className={clsx(classes.ticket__price)}>
+                    {ticket.price} Р
+                  </div>
+                  <img
+                    className={clsx(classes.ticket__img)}
+                    src={`https://pics.avs.io/99/36/${ticket.carrier}.png`}
+                    alt="Airline logo"
+                  />
+                </div>
+                <div className={clsx(classes.ticket__section)}>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      {ticket.segments[0].origin} –{' '}
+                      {ticket.segments[0].destination}
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {formattedTimeRange1}
+                    </div>
+                  </div>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      В пути
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {formattedDuration1}
+                    </div>
+                  </div>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      2 пересадки
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {ticket.segments[0].stops.join(', ')}
+                    </div>
+                  </div>
+                </div>
+
+                <div className={clsx(classes.ticket__section)}>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      {ticket.segments[1].origin} –{' '}
+                      {ticket.segments[1].destination}
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {formattedTimeRange2}
+                    </div>
+                  </div>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      В пути
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {formattedDuration2}
+                    </div>
+                  </div>
+                  <div className={clsx(classes['ticket__section-info'])}>
+                    <div
+                      className={clsx(classes['ticket__section-info--name'])}
+                    >
+                      1 пересадки
+                    </div>
+                    <div
+                      className={clsx(classes['ticket__section-info--value'])}
+                    >
+                      {ticket.segments[1].stops.join(', ')}
+                    </div>
+                  </div>
+                </div>
+              </section>
+            )
+          })}
+        </>
+      )}
+    </>
   )
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -6,31 +6,24 @@ export const fetchTickets = () => async (dispatch) => {
   try {
     const searchIdResponse = await axios.get(`${baseUrl}/search`)
     const { searchId } = searchIdResponse.data
-
     let tickets = []
     let stop = false
-
     while (!stop) {
       try {
         const ticketsResponse = await axios.get(
           `${baseUrl}/tickets?searchId=${searchId}`,
         )
-
-        if (!ticketsResponse.data.ok) {
-          throw new Error('Ошибка при получении билетов')
-        }
-
         const { tickets: newTickets, stop: newStop } = ticketsResponse.data
         tickets = [...tickets, ...newTickets]
         stop = newStop
       } catch (error) {
-        // Игнорирование ошибки net::ERR_CONNECTION_TIMED_OUT
-        if (error.code !== 'ECONNABORTED') {
+        // Игнорирование ошибки net::ERR_CONNECTION_TIMED_OUT и ошибки с кодом 500
+        if (error.code !== 'ECONNABORTED' && error.response.status !== 500) {
           throw error
         }
       }
     }
-
+    console.log('tickets:', tickets) // Вывод всех билетов в консоль
     dispatch({ type: 'FETCH_TICKETS_SUCCESS', payload: tickets })
   } catch (error) {
     dispatch({ type: 'FETCH_TICKETS_FAILURE', payload: error.message })
